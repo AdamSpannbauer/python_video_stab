61927df94acb32929f40ece9490bc21b89c02325
Adam Spannbauer <SpannbauerAdam@gmail.com>
2018-04-20 10:45:13 -0400

    reformat setup

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from distutils.core import setup
 
 setup(name='vidstab',
-      version='0.5',
+      version='0.0.5',
       description='Video Stabilization using OpenCV',
       author='Adam Spannbauer',
       author_email='spannbaueradam@gmail.com',
       url='https://github.com/AdamSpannbauer/python_video_stab',
-      packages=['cv2', 'imutils', 'numpy', 'pandas', 'progress'],
+      packages=['vidstab'],
+      license='MIT',
+      install_requires=[
+          'numpy',
+          'pandas',
+          'imutils',
+          'progress',
+      ],
+      keywords=['video stabilization', 'computer vision', 'image processing', 'opencv']
       )
