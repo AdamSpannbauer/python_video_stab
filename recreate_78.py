62ad0f6bb990ae7e31d529b73ccd05e2340c0e3f
Adam Spannbauer <SpannbauerAdam@gmail.com>
2019-08-05 10:19:20 -0400

    rm bug recreate files

--- recreate_78.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--input', required=True,
-                help='Test to mimic vidstab __main__ input')
-args = vars(ap.parse_args())
-
-print(args)
-print(ap.parse_args())
