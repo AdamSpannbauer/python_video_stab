fix off by one errors

tests/test_frame_queue.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 
+from vidstab.frame import Frame
 from vidstab.frame_queue import FrameQueue
 
 
@@ -27,10 +28,17 @@ def test_set_frame_source():
 
 
 def test_read_frame():
-    frame_queue = FrameQueue()
+    frame_queue = FrameQueue(max_len=1)
     black_frame_image = np.zeros((3, 3, 3), dtype='uint8')
 
-    i, break_flag = frame_queue.read_frame(array=black_frame_image)
+    i, frame, break_flag = frame_queue.read_frame(array=black_frame_image, pop_ind=False)
+
+    assert i is None
+    assert frame is None
+    assert break_flag is None
+
+    i, frame, break_flag = frame_queue.read_frame(array=black_frame_image)
 
     assert i == 0
+    assert isinstance(frame, Frame)
     assert break_flag is None

tests/test_vidstab.py
@@ -6,7 +6,7 @@ import pytest
 
 from vidstab import VidStab
 from vidstab.download_videos import download_ostrich_video, download_truncated_ostrich_video
-from .pickled_transforms import download_pickled_transforms, pickle_test_transforms
+from tests.pickled_transforms import download_pickled_transforms, pickle_test_transforms
 
 # excluding non-free "SIFT" & "SURF" methods do to exclusion from opencv-contrib-python
 # see: https://github.com/skvark/opencv-python/issues/126
@@ -102,3 +102,7 @@ def test_stabilize_frame():
     assert np.allclose(stabilizer.transforms, unpickled_transforms[0])
     assert np.allclose(stabilizer.trajectory, unpickled_transforms[1])
     assert np.allclose(stabilizer.smoothed_trajectory, unpickled_transforms[2])
+
+
+if __name__ == '__main__':
+    test_stabilize_frame()